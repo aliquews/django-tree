355d9482c277ef11b860bfa759c96df6d29cbe45
Alik Kavkazov <kavkazov@hotmail.com>
2023-02-21 22:34:24 +0500

    model

--- website/menuapp/models.py
+++ website/menuapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=True)
+    url = models.CharField(max_length=255, blank=True)
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='child',
+    )
+    is_active = models.BooleanField(default=False)
--- website/menuapp/templatetags/burger-menu.py
+++ website/menuapp/templatetags/burger-menu.py
@@ -1,8 +1,15 @@
+import re
+
 from django import template
+from django.http import HttpRequest
+from django.template import RequestContext
+from django.urls import reverse, NoReverseMatch
+
+from menuapp.models import MenuItem
 
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def draw_menu(request, context):
+def draw_menu(contest: RequestContext):
     pass
